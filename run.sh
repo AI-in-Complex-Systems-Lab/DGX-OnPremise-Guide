1ad120416f3bc7f19b7e3b0d1e7a276591086584
Hakan Tugrul Otal <55956433+hakanotal@users.noreply.github.com>
2025-03-12 11:14:49 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,18 @@
 
 module load slurm
 
+echo -e "Job started..."
+echo -e "================================================================================\n"
+
 sbatch jupyter.sh
 
+sleep 5
+
+port="8008"
+node_id=$(echo $(squeue -u $USER | grep "dgx" | awk '{print $8}'))
 squeue -u $USER
+echo -e "\n================================================================================\n"
+echo -e "Please open a new SSH connection on a separate terminal session and run:\n"
+echo -e "ssh -N -L ${port}::${port} $USER@dgx-head01.its.albany.edu\n"
+echo -e "Then your Jupyter Lab session will be available at (may take couple minutes):\nhttp://${node_id}.its.albany.edu:${port}\n"
+echo -e "================================================================================\n"
