a52a672aeb7e1c74f380ed2e23753c42b5aa6309
Hakan Tugrul Otal <55956433+hakanotal@users.noreply.github.com>
2025-03-27 15:02:33 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ port="8008"
 node_id=$(echo $(squeue -u $USER | grep "dgx" | awk '{print $8}'))
 squeue -u $USER
 echo -e "\n================================================================================\n"
-echo -e "Please open a new SSH connection on a separate terminal session and run:\n"
-echo -e "ssh -N -L ${port}::${port} $USER@dgx-head01.its.albany.edu\n"
-echo -e "Then your Jupyter Lab session will be available at (may take couple minutes):\nhttp://${node_id}.its.albany.edu:${port}\n"
+#echo -e "Please open a new SSH connection on a separate terminal session and run:\n"
+#echo -e "ssh -N -L ${port}::${port} $USER@dgx-head01.its.albany.edu\n"
+echo -e "Your Jupyter Lab session will be available at (may take 5-10 minutes):\nhttp://${node_id}.its.albany.edu:${port}\n"
 echo -e "================================================================================\n"
